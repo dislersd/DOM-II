8d19122ba9ec5a7d436b2c064d886fc4e41f0046
dislersd <dislersd@gmail.com>
2019-01-29 13:11:12 -0800

    added two event listeners mouseover and mouseout

--- js/index.js
+++ js/index.js
@@ -1,1 +1,28 @@
 // Your code goes here
+
+const imgArray = document.querySelectorAll('img');
+
+// img.addEventListener('mouseover', e => {
+//   TweenMax.to(e.currentTarget, 1, {scale: 1.4,ease:Bounce.easeOut});
+// })
+
+// img.addEventListener('mouseout', e => {
+//   TweenMax.to(e.currentTarget, 1, {scale: 1});
+//   e.stopPropagation();
+// })
+
+
+
+imgArray.forEach( elem => {
+  elem.addEventListener('mouseover', e => {
+    TweenMax.to(e.currentTarget, 1, {scale: 1.4,ease:Bounce.easeOut});
+  });
+});
+
+imgArray.forEach( elem => {
+  elem.addEventListener('mouseout', e => {
+    TweenMax.to(e.currentTarget, .8, {scale: 1,ease:Bounce.easeOut});
+  });
+});
+
+
